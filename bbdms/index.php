25fd8eb4744554c9db14ab28f901363056d945b2
vk0810-k <68016633+vk0810-k@users.noreply.github.com>
2021-10-22 19:40:30 +0530

    Update index.php

--- bbdms/index.php
+++ bbdms/index.php
@@ -12,7 +12,7 @@ include('includes/config.php');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>E-BloodBank Management System</title>
+    <title>E-BloodBank Management System | Home</title>
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="css/modern-business.css" rel="stylesheet">

49ab72a9ad49e3de59a22ee0e9161709c2854adb
vk0810-k <68016633+vk0810-k@users.noreply.github.com>
2021-10-22 13:53:08 +0530

    Update footer.php

--- bbdms/includes/footer.php
+++ bbdms/includes/footer.php
@@ -1,8 +1,46 @@
-  
-  
-  <footer class="py-5 bg-inverse">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; BloodBank Management System 2021</p>
-        </div>
-        <!-- /.container -->
-    </footer>
+<footer>
+<div class="container">
+
+<div class="row">
+
+    <div style="  font-size: 3rem;color: #fff;" class="col-md-4">
+        <a style="font-size: 2rem; color: #ccc; display: block;" href="index.php" class="logo"><span>H</span>ealth<span>C</span>are.</a>
+        <p style="font-size: 1.5rem;color: #eee;padding: 1rem 0;">Address: Plot No. C-87, Near Kharghar Cha Raja Ganesh Temple, Navi Mumbai.
+                       Phone: 254-296-8200
+        Email: ebloodbank@info.com
+
+        </p>
+    </div>
+
+    <div class="col-md-4 text-center">
+        <h3 style="color: #fff;font-size: 2rem;">Navigation Links</h3>
+        <a style="font-size: 2rem; color: #ccc; display: block;" href="index.php">Home</a>
+        <a style="font-size: 2rem; color: #ccc; display: block;" href="become-donar.php">Become a Donor</a>
+        <a style="font-size: 2rem; color: #ccc; display: block;" href="search-donor.php">Search Blood</a>
+        <a style="font-size: 2rem; color: #ccc; display: block;" href="contact.php">Contact Us</a>
+    </div>
+
+    <div style="padding: 1.3rem;" class="col-md-4 text-center">
+        <h3 style="color: #fff;font-size: 2rem;">Share on</h3>
+        <a style="font-size: 2rem; color: #ccc; display: block;" href="https://github.com/CryptoLeo-3000/BloodBank.git">Github</a>
+    </div>
+</div>
+</div>
+    <div class="container">
+        <p style="font-size: 1.5rem;color: #eee;padding: 1rem 0;"class="m-0 text-center text-white">
+            Copyright &copy; E-BloodBank Management System 2021
+        </p>
+    </div>
+</footer>
+
+<style>
+footer {
+    display: block;
+    height: 345px;
+    text-align: center;
+    padding: 3px;
+    background-color: #292b2c;
+    color: white;
+}
+
+</style>

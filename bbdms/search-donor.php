00a5cee56846225cdc011d98ef33911e8b6f5257
Varun Mahendra Khadayate <68016633+vk0810-k@users.noreply.github.com>
2021-10-24 09:15:01 +0530

    Update search-donor.php

--- bbdms/search-donor.php
+++ bbdms/search-donor.php
@@ -59,7 +59,7 @@ include('includes/config.php');
     <div class="container">
 
         <!-- Page Heading/Breadcrumbs -->
-        <h1 class="mt-4 mb-3">Search <small>Donor</small></h1>
+        <h1 class="mt-4 mb-3">Search <small>Blood and Donor</small></h1>
 
         <ol class="breadcrumb">
             <li class="breadcrumb-item">

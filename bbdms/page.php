d41efc905589f8882ec3369753a03b4d7045dbf6
Varun Mahendra Khadayate <68016633+vk0810-k@users.noreply.github.com>
2021-10-24 09:15:28 +0530

    Update page.php

--- bbdms/page.php
+++ bbdms/page.php
@@ -14,7 +14,7 @@ include('includes/config.php');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Modern Business - Start Bootstrap Template</title>
+    <title>E-BloodBank Management System | Home</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
